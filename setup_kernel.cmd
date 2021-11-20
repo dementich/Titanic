c361236e4127c8df1035a6daf4f1effbe8bafa8a
dementich <cyril.bulanov@gmail.com>
2021-11-20 11:47:19 +0300

    first commit

---
+++ setup_kernel.cmd
@@ -0,0 +1,10 @@
+rem ipython kernel install --name "env" --user
+rem .\env\Scripts\activate.bat
+pip install jupyter;
+pip install numpy;
+pip install pandas;
+pip install sklearn;
+pip install xgboost;
+pip install lightgbm;
+pip install catboost;
+rem ipython kernel install --name "env" --user
